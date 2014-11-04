95444c703758e4180dca82ca4b052754834e9855
Pierre Krieger <pierre.krieger1708@gmail.com>
2014-11-04 08:28:14 +0100

    Make it compile!

--- libogg/build.rs
+++ libogg/build.rs
@@ -1,8 +1,8 @@
 extern crate gcc;
 
 fn main() {
-    let root = Path::new(std::os::getenv("CARGO_MANIFEST_DIR").unwrap()
-        .join("libogg").join("lib");
+    let root = Path::new(std::os::getenv("CARGO_MANIFEST_DIR").unwrap())
+        .join("libogg");
 
     let config = gcc::Config {
         include_directories: vec![
@@ -13,14 +13,14 @@ fn main() {
             ("LIBOGG_EXPORTS".to_string(), None)
         ],
         .. std::default::Default::default()
-    }
+    };
 
-    println!("cargo:include={}", root.join("include"));
+    println!("cargo:include={}", root.join("include").display());
 
     let libfiles_root = root.join("src");
 
     gcc::compile_library("libogg.a", &config, &[
-        libfiles_root.join("bitwise.c"),
-        libfiles_root.join("framing.c"),
+        libfiles_root.join("bitwise.c").as_str().unwrap(),
+        libfiles_root.join("framing.c").as_str().unwrap(),
     ]);
 }
