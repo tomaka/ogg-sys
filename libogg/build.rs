415bebcaba2eb309b747de8f73f1ea811ba93326
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-02-12 13:40:30 +0100

    Update for libc and gcc

--- libogg/build.rs
+++ libogg/build.rs
@@ -10,21 +10,11 @@ fn main() {
     let root = Path::new(std::os::getenv("CARGO_MANIFEST_DIR").unwrap())
         .join("libogg");
 
-    let config = gcc::Config {
-        include_directories: vec![
-            root.join("include"),
-        ],
-        definitions: vec![
-            ("_USRDLL".to_string(), None),
-            ("LIBOGG_EXPORTS".to_string(), None)
-        ],
-        .. std::default::Default::default()
-    };
-
-    println!("cargo:include={}", root.join("include").display());
-
-    gcc::compile_library("libogg.a", &config, &[
-        "libogg/src/bitwise.c",
-        "libogg/src/framing.c",
-    ]);
+    gcc::Config::new()
+                .file("libogg/src/bitwise.c")
+                .file("libogg/src/framing.c")
+                .define("_USRDLL", None)
+                .define("LIBOGG_EXPORTS", None)
+                .include(root.join("include"))
+                .compile("libogg.a");
 }
